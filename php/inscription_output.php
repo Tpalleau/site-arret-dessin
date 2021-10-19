c23c8fe39c4041d42367c5d6ec727ffaba789293
Tom P <tom.palleau@gmail.com>
2021-10-19 19:50:42 +0200

    sql->[mdp crypté, ajout de date de naissance, niv ajouté]

--- php/inscription_output.php
+++ php/inscription_output.php
@@ -1,19 +1,28 @@
 <?php
-echo "enregistrement réussi";
+echo 'enregistrement réussi ' . $_POST["niveau"];
 $db = new PDO("mysql:host=localhost; dbname=dessin_bdd;charset=UTF8","root","");
 
+//$req_lvl_id = $db->prepare("select id from nom_niv where nom=?");
+//$req_lvl_id->execute([$_POST["niveau"]]);
+//$lvl = $req_lvl_id->fetch();
+//echo $lvl;
+
 $add_user = $db->prepare("insert into utilisateur(
-                         email,
-                         surnom,
-                         nom,
-                         prenom,
-                         passwd
-) values (?,?,?,?,?)");
+                        email,
+                        surnom,
+                        nom,
+                        prenom,
+                        passwd,
+                        naissance,
+                        niv_dessin
+) values (?,?,?,?,?,?, (select id from nom_niv where nom=?))");
 
 $add_user->execute(
     [$_POST["texte_email"],
     $_POST["texte_surnom"],
     $_POST["texte_nom"],
     $_POST["texte_prenom"],
-    $_POST["mdp"]]
+    password_hash($_POST["mdp"], PASSWORD_DEFAULT),
+    $_POST["date_naissance"],
+    $_POST["niveau"]]
 );
