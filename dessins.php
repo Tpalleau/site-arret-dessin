c0bab500a39925b961e251fe2c3e7ce1673d0028
gregmetral <66792724+gregmetral@users.noreply.github.com>
2021-10-19 14:54:43 +0200

    creation page en html

---
+++ dessins.php
@@ -0,0 +1,29 @@
+<html lang="fr">
+    <head>
+        <title>Dessins</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1> ARRET DESSIN </h1>
+        </header>
+        <nav>
+            <div>
+                <ul>
+                    <li> <a href="connexion.php">Connexion</a> </li> <!-- menu deroulant inscription ou se connecter -->
+                        <ul>
+                            <li> <a href="connexion.php">Se Connecter</a> </li>
+                            <li> <a href="inscription.php">S'Inscrire</a> </li>
+                        </ul>
+                    <li> <a href="cours.php">Cours</a> </li>
+                    <li> <a href="dessins.php">Dessins</a> </li>
+                    <li> <a href="demande.php">Demande au club</a> </li> <!-- demande materiel, cours ou autre -->
+                </ul>
+            </div>
+        </nav>
+        <footer>
+            <p>Arret dessin </p>
+            <img src="logo.png" width="120" height="118">
+        </footer>
+    </body>
+</html>
