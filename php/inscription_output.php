4e522edc31654ddae0c68edc3a080b3b2b070d9b
Tom P <tom.palleau@gmail.com>
2021-10-19 20:13:18 +0200

    ne peux pas avoir plusieurs utilisateur avec le même [nom, prenom, surnom, email]

--- php/inscription_output.php
+++ php/inscription_output.php
@@ -1,28 +1,44 @@
 <?php
-echo 'enregistrement réussi ' . $_POST["niveau"];
 $db = new PDO("mysql:host=localhost; dbname=dessin_bdd;charset=UTF8","root","");
 
-//$req_lvl_id = $db->prepare("select id from nom_niv where nom=?");
-//$req_lvl_id->execute([$_POST["niveau"]]);
-//$lvl = $req_lvl_id->fetch();
-//echo $lvl;
-
-$add_user = $db->prepare("insert into utilisateur(
-                        email,
-                        surnom,
-                        nom,
-                        prenom,
-                        passwd,
-                        naissance,
-                        niv_dessin
-) values (?,?,?,?,?,?, (select id from nom_niv where nom=?))");
-
-$add_user->execute(
+//verifie que l'utilsateur n'existe pas déjà
+$req_user_exist = $db->prepare("select * from utilisateur where
+email=? or
+nom=? or
+prenom=? or
+surnom=?"
+);
+$req_user_exist->execute(
     [$_POST["texte_email"],
-    $_POST["texte_surnom"],
     $_POST["texte_nom"],
     $_POST["texte_prenom"],
-    password_hash($_POST["mdp"], PASSWORD_DEFAULT),
-    $_POST["date_naissance"],
-    $_POST["niveau"]]
+    $_POST["texte_surnom"]]
 );
+$user_exist = $req_user_exist->fetch();
+
+//si il n'existe pas il est ajouté à la bdd
+if ($user_exist == NULL) {
+
+    $add_user = $db->prepare("insert into utilisateur(
+                            email,
+                            surnom,
+                            nom,
+                            prenom,
+                            passwd,
+                            naissance,
+                            niv_dessin
+    ) values (?,?,?,?,?,?, (select id from nom_niv where nom=?))");
+
+    $add_user->execute(
+        [$_POST["texte_email"],
+            $_POST["texte_surnom"],
+            $_POST["texte_nom"],
+            $_POST["texte_prenom"],
+            password_hash($_POST["mdp"], PASSWORD_DEFAULT),
+            $_POST["date_naissance"],
+            $_POST["niveau"]]
+    );
+    echo 'user created';
+}else{
+    echo 'user already exists';
+}
