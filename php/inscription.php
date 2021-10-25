55e5587cd6dd87a37f14df8b6da6b1d39e86b03b
gregmetral <66792724+gregmetral@users.noreply.github.com>
2021-10-25 15:43:28 +0200

    inscription changements pour css

--- php/inscription.php
+++ php/inscription.php
@@ -23,16 +23,16 @@
             <form action="inscription_output.php" method="post">
                 <h2>S'Inscrire</h2>
                 <label>
-                    Nom : <input type="text" name="texte_nom" id="texte_nom" required="required">
+                    Nom : <input class="texte" type="text" name="texte_nom" id="texte_nom" required="required">
                 </label> <br/>
                 <label>
-                    Prenom : <input type="text" name="texte_prenom" id="texte_prenom" required="required">
+                    Prenom : <input class="texte" type="text" name="texte_prenom" id="texte_prenom" required="required">
                 </label><br>
                 <label>
-                    Surnom : <input type="text" name="texte_surnom" id="texte_surnom" placeholder="surnom UTBM" required="required">
+                    Surnom : <input class="texte" type="text" name="texte_surnom" id="texte_surnom" placeholder="surnom UTBM" required="required">
                 </label><br>
                 <label>
-                    Email : <input type="email" name="texte_email" id="texte_email" placeholder="exemple@mail.fr" required="required">
+                    Email : <input class="texte" type="email" name="texte_email" id="texte_email" placeholder="exemple@mail.fr" required="required">
                 </label><br>
                 <label for="niveau">Niveau de Dessin : </label>
                 <select name="niveau" id="niveau">
@@ -51,10 +51,10 @@
                     Date de Naissance : <input type="date" name="date_naissance" id="date_naissance" required="required">
                 </label><br>
                 <label>
-                    Mot de Passe : <input type="password" name="mdp" id="mdp" required="required">
+                    Mot de Passe : <input class="texte" type="password" name="mdp" id="mdp" required="required">
                 </label><br>
                 <label>
-                    <input type="submit" value="S'inscrire">
+                    <input class = "submit" type="submit" value="S'inscrire">
                 </label>
             </form>
         </div>
