b70e19c06b69b8010377558ce3e72d86fee74086
Tom <ptitsawyer@gmail.com>
2021-10-29 16:57:58 +0200

    toute page affiche selon si la personne est connecter & page cours possède les boutons necessaire

--- php/cours.php
+++ php/cours.php
@@ -1,26 +1,77 @@
 <html lang="fr">
-    <head>
-        <title>Cours</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <header>
-            <h1> ARRET DESSIN </h1>
-        </header>
-        <nav>
-            <div class="menu">
-                <ul>
-                    <li> <a href="inscription.php">S'Inscrire</a> </li>
-                    <li> <a href="connexion.php">Connexion</a> </li>
-                    <li> <a href="cours.php">Cours</a> </li>
-                    <li> <a href="dessins.php">Dessins</a> </li>
-                    <li> <a href="demande.php">Demande au club</a> </li> <!-- demande materiel, cours ou autre -->
-                </ul>
-            </div>
-        </nav>
-        <footer>
-            <p>Arret dessin </p>
-        </footer>
-    </body>
+<?php
+session_start();
+?>
+<head>
+    <title>Index</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <h1> ARRET DESSIN </h1>
+</header>
+<nav>
+    <div class="menu">
+        <ul>
+            <?php
+            if (!isset($_SESSION["connected"]) || !$_SESSION["connected"]) {
+                ?>
+                <li> <a href="inscription.php">S'Inscrire</a> </li>
+                <li> <a href="connexion.php">Connexion</a> </li>
+                <?php
+            }else{ ?>
+                <li> <a href="connexion_output.php?connect=false">deconnnexion</a> </li>
+
+                <?php
+            } ?>
+            <li> <a href="cours.php">Cours</a> </li>
+            <li> <a href="dessins.php">Dessins</a> </li>
+            <li> <a href="demande.php">Demande au club</a> </li> <!-- demande materiel, cours ou autre -->
+        </ul>
+    </div>
+</nav>
+<div class="recherche_cours">
+    <form>
+        <h2>cours</h2>
+        <label for="cour">cour : </label>
+        <select name="cour" id="cour">
+
+            <?php
+            $db = new PDO("mysql:host=localhost;dbname=dessin_bdd;charset=UTF8","root","");
+            $req_name_cour = $db->prepare("select nom, id from nom_cours");
+            $req_name_cour->execute();
+
+            while ($name = $req_name_cour->fetch())
+                echo "<option value=" . $name["id"] . ">" . $name["nom"] . "</option>";
+            ?>
+
+        </select>
+        <label>
+            date : <input type="date" name="date_cour" id="date_cour" required="required">
+        </label>
+
+        <label for="time">heure </label>
+        <select name="time" id="time">
+
+            <?php
+            for ($heure=8; $heure<=18; ++$heure){
+                if ($heure!=12){
+                    echo "<option value=" . $heure . ">" . $heure . "h</option>";
+                }
+            }
+            ?><br>
+
+        </select><br>
+        <label>
+
+        <label><br>
+            <input class = "submit" type="search" value="search">
+        </label>
+    </form>
+</div>
+<footer>
+    <p>Arret dessin </p>
+</footer>
+</body>
 </html>
--- php/demande.php
+++ php/demande.php
@@ -1,26 +1,38 @@
 <html lang="fr">
-    <head>
-        <title>Demande</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <header>
-            <h1> ARRET DESSIN </h1>
-        </header>
-        <nav>
-            <div class="menu">
-                <ul>
-                    <li> <a href="inscription.php">S'Inscrire</a> </li>
-                    <li> <a href="connexion.php">Connexion</a> </li>
-                    <li> <a href="cours.php">Cours</a> </li>
-                    <li> <a href="dessins.php">Dessins</a> </li>
-                    <li> <a href="demande.php">Demande au club</a> </li> <!-- demande materiel, cours ou autre -->
-                </ul>
-            </div>
-        </nav>
-        <footer>
-            <p>Arret dessin </p>
-        </footer>
-    </body>
+<?php
+session_start();
+?>
+<head>
+    <title>Index</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <h1> ARRET DESSIN </h1>
+</header>
+<nav>
+    <div class="menu">
+        <ul>
+            <?php
+            if (!isset($_SESSION["connected"]) || !$_SESSION["connected"]) {
+                ?>
+                <li> <a href="inscription.php">S'Inscrire</a> </li>
+                <li> <a href="connexion.php">Connexion</a> </li>
+                <?php
+            }else{ ?>
+                <li> <a href="connexion_output.php?connect=false">deconnnexion</a> </li>
+
+                <?php
+            } ?>
+            <li> <a href="cours.php">Cours</a> </li>
+            <li> <a href="dessins.php">Dessins</a> </li>
+            <li> <a href="demande.php">Demande au club</a> </li> <!-- demande materiel, cours ou autre -->
+        </ul>
+    </div>
+</nav>
+<footer>
+    <p>Arret dessin </p>
+</footer>
+</body>
 </html>
